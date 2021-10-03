7df114463bf889510796ca7ec02f6dc41d47d77c
maxwel <maxwelfcoelho@gmail.com>
2021-10-03 09:19:14 -0300

    add cors

--- server/app.js
+++ server/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
 
 app.get('/random-hand', (req, res) => {
     const hands = ['pedra', 'papel', 'tesoura'];
